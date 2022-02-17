e18edf6d75cf96adc2acac4dc74e9a7c509677ef
Francisco J Garrido <fragarflo2@alum.us.es>
2022-02-17 20:29:47 +0100

    drone moving

--- src/dronecontrol.cpp
+++ src/dronecontrol.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose.h>
 #include <gazebo_msgs/ModelState.h>
+#include <gazebo_msgs/ModelStates.h>
 #include <rms/TakeOff.h>
 #include <rms/GoToWaypoint.h>
-#include "../include/rms/dronecontrol.hpp"
 
 bool landed = true;
 bool busy = false;
+bool moving = false;
 
 std::string robotName = "quadrotor";
 geometry_msgs::Pose currentPose;
+geometry_msgs::Pose targetPose;
 geometry_msgs::Twist currentVelocity;
+geometry_msgs::Twist targetVelocity;
 
-double frequency = 60.;
+double frequency = 120.;
 
 void GazeboSubscriber(const gazebo_msgs::ModelStates::ConstPtr& msg)
 {
@@ -28,104 +33,84 @@ void GazeboSubscriber(const gazebo_msgs::ModelStates::ConstPtr& msg)
     }
 }
 
-bool DroneControl::TakeOff(double height)
+bool TakeOffServiceCalled(rms::TakeOff::Request& req, rms::TakeOff::Response& res)
 {
     if (landed && !busy)
     {
         targetPose = currentPose;
-        targetPose.position.z = height;
+        targetPose.position.z = req.height;
+        res.ok = true;
+        landed = false;
         return true;
     }
     else
     {
+        res.ok = false;
         return false;
     }
 }
 
-bool DroneControl::GoToWaypoint(const geometry_msgs::Point& waypoint)
+bool GoToWaypointServiceCalled(rms::GoToWaypoint::Request& req, rms::GoToWaypoint::Response& res)
 {
-    if(!busy)
+    if(!landed && !busy)
     {
-        targetPose.position = waypoint;
+        targetPose.position = req.waypoint;
+        res.ok = true;
         return true;
     }
     else
     {
+        res.ok = false;
         return false;
     }
 }
 
-geometry_msgs::Twist DroneControl::GetVelocityRef()
+geometry_msgs::Twist GetVelocityRef(geometry_msgs::Point targetPoint)
 {
-    geometry_msgs::Pose poseError;
-    poseError.position.x = targetPose.position.x - currentPose.position.x;
-    poseError.position.y = targetPose.position.y - currentPose.position.y;
-    poseError.position.z = targetPose.position.z - currentPose.position.z;
-    double distance = sqrt(pow(poseError.position.x, 2) + pow(poseError.position.y, 2) + pow(poseError.position.z, 2));
+    double max_horizontal_velocity = 10.0;
+    double max_vertical_velocity = 10.0;
+    double max_position_error = 0.001;
+    geometry_msgs::Twist vel;
 
-    //w = w + Kp*vQ + (Ki*Tsampling - Kp)*pVQ; // Forward Euler (PI)
-    geometry_msgs::Twist velocity;
-    velocity.linear.x = currentVelocity.linear.x + kp*poseError.position.x + (ki*(1./30.) - kp)*lastError;
-    velocity.linear.y = currentVelocity.linear.y + kp*poseError.position.y + (ki*(1./30.) - kp)*lastError;
-    velocity.linear.z = currentVelocity.linear.z + kp*poseError.position.z + (ki*(1./30.) - kp)*lastError;
+    double dx = targetPoint.x - currentPose.position.x;
+    double dy = targetPoint.y - currentPose.position.y;
+    double dz = targetPoint.z - currentPose.position.z;
 
-    if (velocity.linear.x > 10.)
-        velocity.linear.x = 10.;
-    if (velocity.linear.y > 10.)
-        velocity.linear.y = 10.;
-    if (velocity.linear.z > 10.)
-        velocity.linear.z = 10.;
+    double Th = sqrt( dx*dx + dy*dy ) / max_horizontal_velocity;
+    double Tz = std::abs( dz / max_vertical_velocity);
+    double T = std::max(Th, Tz);
 
-    return velocity;
+    if ( T < 1/frequency ) 
+        T = 1/frequency;
+
+    vel.linear.x = dx / T;
+    vel.linear.y = dy / T;
+    vel.linear.z = dz / T;
+
+    if (std::abs(dx) < max_position_error)
+        vel.linear.x = 0;
+    if (std::abs(dy) < max_position_error)
+        vel.linear.y = 0;
+    if (std::abs(dz) < max_position_error)
+        vel.linear.z = 0;
+
+    return vel;
 }
 
-geometry_msgs::Pose DroneControl::Move(geometry_msgs::Twist refVelocity)
+geometry_msgs::Pose Move()
 {
-    double dt = 1 / 30.;
+    double dt = 1./frequency;
 
-    currentVelocity.linear.x = (0.2 * refVelocity.linear.x + 0.8 * currentVelocity.linear.x);
-    currentVelocity.linear.y = (0.2 * refVelocity.linear.y + 0.8 * currentVelocity.linear.y);
-    currentVelocity.linear.z = (0.2 * refVelocity.linear.z + 0.8 * currentVelocity.linear.z);
+    currentVelocity.linear.x = (0.4 * targetVelocity.linear.x + 0.6 * currentVelocity.linear.x);
+    currentVelocity.linear.y = (0.4 * targetVelocity.linear.y + 0.6 * currentVelocity.linear.y);
+    currentVelocity.linear.z = (0.4 * targetVelocity.linear.z + 0.6 * currentVelocity.linear.z);
 
     geometry_msgs::Pose pose;
-
-    pose.position.x += dt * currentVelocity.linear.x;
-    pose.position.y += dt * currentVelocity.linear.y;
-    pose.position.z += dt * currentVelocity.linear.z;
+    pose.position.x = currentPose.position.x + dt * currentVelocity.linear.x;
+    pose.position.y = currentPose.position.y + dt * currentVelocity.linear.y;
+    pose.position.z = currentPose.position.z + dt * currentVelocity.linear.z;
 
     return pose;
-}
-
-DroneControl::DroneControl()
-{
-    ros::NodeHandle nh;
-
-    ros::ServiceServer takeOffService = nh.advertiseService<rms::TakeOff::Request, rms::TakeOff::Response>("/take_off",
-        [this](rms::TakeOff::Request& req, rms::TakeOff::Response& res) 
-        { res.ok = true; return TakeOff(req.height); });
-
-    ros::ServiceServer goToWaypointService = nh.advertiseService<rms::GoToWaypoint::Request, rms::GoToWaypoint::Response>("/go_to_waypoint",
-        [this](rms::GoToWaypoint::Request& req, rms::GoToWaypoint::Response& res) 
-        { res.ok = true; return GoToWaypoint(req.waypoint); });
-
-    ros::Publisher publisher = nh.advertise<gazebo_msgs::ModelStates>("/gazebo/model_states", 10);
-
-    ros::Rate rate(30.);
-    while (ros::ok())
-    {
-        geometry_msgs::Twist velocityRef = GetVelocityRef();
-        geometry_msgs::Pose nextPose = Move(velocityRef);
-
-        gazebo_msgs::ModelState msg;
-        msg.model_name = robotName;
-        msg.pose = nextPose;
-
-        publisher.publish(msg);
-
-        ros::spinOnce();
-        rate.sleep();
-    }
-    
 }
 
 int main(int argc, char* argv[])
@@ -133,16 +118,30 @@ int main(int argc, char* argv[])
     ros::init(argc, argv, "drone_control");
 
     ros::service::waitForService("/gazebo/spawn_sdf_model");
-    DroneControl control = DroneControl();
 
     ros::NodeHandle nh;
     ros::Subscriber gazeboSubscriber = nh.subscribe<gazebo_msgs::ModelStates>("/gazebo/model_states", 10, GazeboSubscriber);
 
+    ros::Publisher publisher = nh.advertise<gazebo_msgs::ModelState>("/gazebo/set_model_state", 10);
+
+    ros::ServiceServer takeOffService = nh.advertiseService("/take_off", TakeOffServiceCalled);
+    ros::ServiceServer goToWaypointService = nh.advertiseService("/go_to_waypoint", GoToWaypointServiceCalled);
+
+    ros::Rate rate(frequency);
     while(ros::ok())
     {
-        control.currentPose = currentPose;
-        control.currentVelocity = currentVelocity;
+        targetVelocity = GetVelocityRef(targetPose.position);
 
+        gazebo_msgs::ModelState msg;
+        msg.model_name = robotName;
+        msg.pose = Move();
+
+        ROS_INFO("Moving to %f,%f,%f", msg.pose.position.x, msg.pose.position.y, msg.pose.position.z);
+        ROS_INFO("Ref Speed %f,%f,%f", targetVelocity.linear.x, targetVelocity.linear.y, targetVelocity.linear.z);
+
+        publisher.publish(msg);
+        
         ros::spinOnce();
+        rate.sleep();
     }
 }
