3d0e12050421b7de71bc747f012ee4261b58609b
Francisco J Garrido <fragarflo2@alum.us.es>
2022-02-19 20:42:27 +0100

    ekf 1.0

--- scripts/ekf.py
+++ scripts/ekf.py
@@ -21,16 +21,17 @@ class EKF:
 
         self.beaconPoses = {}
         self.beaconDists = {}
+        self.beaconDistsOld = {}
 
         self.state = 0
         self.freq = 10.
         T = 1./self.freq
-        q = 0.01
-        rx = 16.
-        ry = 16.
-        rz = 36.
+        q = 0.001
+        rx = 9.
+        ry = 9.
+        rz = 25.
         rb = 9.
-        rh = 0.001
+        rh = 0.025
 
         self.F = np.matrix([[1,  0,  0,  T,  0,  0],
                             [0,  1,  0,  0,  T,  0],
@@ -69,7 +70,7 @@ class EKF:
                             [0,  0,  rz, 0,  0,  0],
                             [0,  0,  0,  rx, 0,  0],
                             [0,  0,  0,  0,  ry, 0],
-                            [0,  0,  0,  0,  0,  rz] ])
+                            [0,  0,  0,  0,  0,  rz]])
 
         self.X = np.matrix([[0.],
                             [0.],
@@ -148,30 +149,66 @@ class EKF:
                            [self.heightData],
                            [self.heightDataOld]])
 
-            if(self.beaconDists): # If beacon measures
+            y = np.matrix([[0.],
+                            [0.],
+                            [0.],
+                            [0.],
+                            [0.],
+                            [0.],
+                            [0.],
+                            [0.]]) # Innovation matrix
+            y[0][0] = X_p[0][0] # xk-1
+            y[1][0] = X_p[1][0] # yk-1
+            y[2][0] = X_p[2][0] # zk-1
+            y[3][0] = X_p[0][0] - X_p[3][0]*T # xk-1 - vxk-1·T
+            y[4][0] = X_p[1][0] - X_p[4][0]*T # yk-1 - vyk-1·T
+            y[5][0] = X_p[2][0] - X_p[5][0]*T # zk-1 - vzk-1·T
+            y[6][0] = X_p[2][0] # zk-1
+            y[7][0] = X_p[2][0] # zk-1 - vzk-1·T
+
+            if(self.beaconDists): # If beacons
                 for key, value in self.beaconDists.items():
                     z = np.vstack([z, np.matrix([value])])
                     
-                    x = float(self.X[0][0])
-                    y = float(self.X[1][0])
-                    z = float(self.X[2][0])
+                    xk = float(X_p[0][0])
+                    yk = float(X_p[1][0])
+                    zk = float(X_p[2][0])
+                    vx = float(X_p[3][0])
+                    vy = float(X_p[4][0])
+                    vz = float(X_p[5][0])
                     xb = float((self.beaconPoses[key])[0])
                     yb = float((self.beaconPoses[key])[1])
                     zb = float((self.beaconPoses[key])[2])
-                    self.H = np.vstack([self.H, np.array([np.real((2*x - 2*xb)/(2*sqrt((x-xb)**2 + (y-yb)**2 + (z-zb)**2))), 
-                                                           np.real((2*y - 2*yb)/(2*sqrt((x-xb)**2 + (y-yb)**2 + (z-zb)**2))),
-                                                           np.real((2*z - 2*zb)/(2*sqrt((x-xb)**2 + (y-yb)**2 + (z-zb)**2))), 0, 0, 0])])
+                    self.H = np.vstack([self.H, np.array([  np.real((xk - xb)/(sqrt((xk-xb)**2 + (yk-yb)**2 + (zk-zb)**2))), 
+                                                            np.real((yk - yb)/(sqrt((xk-xb)**2 + (yk-yb)**2 + (zk-zb)**2))),
+                                                            np.real((zk - zb)/(sqrt((xk-xb)**2 + (yk-yb)**2 + (zk-zb)**2))), 0, 0, 0])])
+
+                    y = np.vstack([y, np.matrix([sqrt((xk - xb)**2 + (yk - yb)**2 + (zk - zb)**2)])]) # sqrt((xk - xB)² + (yk - yB)² + (zk - zB)²)
+
+                    if key in self.beaconDistsOld:
+                        z = np.vstack([z, np.matrix([self.beaconDistsOld[key]])])
+
+                        self.H = np.vstack([self.H, np.array([  np.real(-(xb-xk+2*T*vx)/(sqrt((xb-xk+2*T*vx)**2 + (yb-yk+2*T*vy)**2 + (zb-zk+2*T*vz)**2))), 
+                                                                np.real(-(yb-yk+2*T*vy)/(sqrt((xb-xk+2*T*vx)**2 + (yb-yk+2*T*vy)**2 + (zb-zk+2*T*vz)**2))),
+                                                                np.real(-(zb-zk+2*T*vz)/(sqrt((xb-xk+2*T*vx)**2 + (yb-yk+2*T*vy)**2 + (zb-zk+2*T*vz)**2))),
+                                                                np.real((2*T*(xb-xk+2*T*vx))/(sqrt((xb-xk+2*T*vx)**2 + (yb-yk+2*T*vy)**2 + (zb-zk+2*T*vz)**2))),
+                                                                np.real((2*T*(yb-yk+2*T*vy))/(sqrt((xb-xk+2*T*vx)**2 + (yb-yk+2*T*vy)**2 + (zb-zk+2*T*vz)**2))),
+                                                                np.real((2*T*(zb-zk+2*T*vz))/(sqrt((xb-xk+2*T*vx)**2 + (yb-yk+2*T*vy)**2 + (zb-zk+2*T*vz)**2)))])])
+
+                        y = np.vstack([y, np.matrix([sqrt((xk -2*T*vx - xb)**2 + (yk -2*T*vy - yb)**2 + (zk -2*T*vz - zb)**2)])]) # sqrt((x-2*T*vx - xB)² + (y-2*T*vy - yB)² + (z-2*T*vz - zB)²)
+                    
+                        self.R = np.pad(self.R, ((0,1), (0,1)), mode='constant', constant_values=0)
+                        self.R[len(self.R)-1][len(self.R)-1] = rb
+
                     self.R = np.pad(self.R, ((0,1), (0,1)), mode='constant', constant_values=0)
                     self.R[len(self.R)-1][len(self.R)-1] = rb
+                    self.beaconDistsOld[key] = self.beaconDists[key]
 
-            #  [(2*x - 2*xb1)/(2*((x - xb1)^2 + (y - yb1)^2 + (z - zb1)^2)^(1/2)),
-            #   (2*y - 2*yb1)/(2*((x - xb1)^2 + (y - yb1)^2 + (z - zb1)^2)^(1/2)),
-            #   (2*z - 2*zb1)/(2*((x - xb1)^2 + (y - yb1)^2 + (z - zb1)^2)^(1/2)),  0,  0,  0]       
+            y = z - y
+            S = np.linalg.inv(np.matmul(np.matmul(self.H, P_p), np.transpose(self.H)) + self.R)
+            K = np.matmul(np.matmul(P_p, np.transpose(self.H)), S)
 
-            temp = np.linalg.inv(np.matmul(np.matmul(self.H, P_p), np.transpose(self.H)) + self.R)
-            K = np.matmul(np.matmul(P_p, np.transpose(self.H)), temp)
-
-            self.X = X_p + np.matmul(K, z - np.matmul(self.H, X_p))
+            self.X = np.real(X_p + np.matmul(K, y))
             self.P = P_p - np.matmul(np.matmul(K, self.H), P_p)
 
             #
--- src/rvizhelper.cpp
+++ src/rvizhelper.cpp
@@ -5,11 +5,14 @@
 #include <gazebo_msgs/ModelState.h>
 #include <gazebo_msgs/ModelStates.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <rms/BeaconMsg.h>
+#include <visualization_msgs/Marker.h>
 
 std::string robotName = "quadrotor";
 ros::Publisher posePublisher;
 ros::Publisher ekfPublisher;
 ros::Publisher gpsPublisher;
+ros::Publisher beaconPublisher;
 
 void EKFCallback(const gazebo_msgs::ModelState::ConstPtr& msg)
 {
@@ -25,6 +28,38 @@ void GPSCallback(const geometry_msgs::Point::ConstPtr& msg)
     ps.point = *msg;
     ps.header.frame_id = "map";
     gpsPublisher.publish(ps);
+}
+
+void BeaconCallback(const rms::BeaconMsg::ConstPtr& msg)
+{
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "map";
+    marker.header.stamp = ros::Time::now();
+    marker.ns = "beacon_" + std::to_string(msg->id);
+    marker.id = msg->id;
+    marker.type = visualization_msgs::Marker::SPHERE;
+    marker.pose.position.x = msg->point.x;
+    marker.pose.position.y = msg->point.y;
+    marker.pose.position.z = msg->point.z;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+
+    // Set the scale of the marker -- 1x1x1 here means 1m on a side
+    marker.scale.x = 2.0 * msg->dist;
+    marker.scale.y = 2.0 * msg->dist;
+    marker.scale.z = 2.0 * msg->dist;
+  
+    // Set the color -- be sure to set alpha to something non-zero!
+    marker.color.r = 0.2f;
+    marker.color.g = 1.0f;
+    marker.color.b = 0.5f;
+    marker.color.a = 0.15f;
+  
+    marker.lifetime = ros::Duration();
+
+    beaconPublisher.publish(marker);
 }
 
 void GazeboCallback(const gazebo_msgs::ModelStates::ConstPtr& msg)
@@ -52,10 +87,12 @@ int main(int argc, char* argv[])
     ros::Subscriber ekfSubscriber = nh.subscribe<gazebo_msgs::ModelState>("/ekf", 10, EKFCallback);
     ros::Subscriber poseSubscriber = nh.subscribe<gazebo_msgs::ModelStates>("/gazebo/model_states", 10, GazeboCallback);
     ros::Subscriber gpsSubscriber = nh.subscribe<geometry_msgs::Point>("/gps", 10, GPSCallback);
+    ros::Subscriber beaconSubscriber = nh.subscribe<rms::BeaconMsg>("/beacon", 10, BeaconCallback);
 
     posePublisher = nh.advertise<geometry_msgs::PoseStamped>("/drone_pose", 10);
     ekfPublisher = nh.advertise<geometry_msgs::PoseStamped>("/drone_ekf", 10);
-    gpsPublisher = nh.advertise<geometry_msgs::PointStamped>("drone_gps", 10);
+    gpsPublisher = nh.advertise<geometry_msgs::PointStamped>("/drone_gps", 10);
+    beaconPublisher = nh.advertise<visualization_msgs::Marker>("/drone_beacon", 10);
 
     ros::spin();
 }
