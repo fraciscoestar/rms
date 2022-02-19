21b9ffe37f8834d3cfca703d794d5aa8723ff7df
Francisco J Garrido <fragarflo2@alum.us.es>
2022-02-19 13:36:32 +0100

    ekf

--- scripts/ekf.py
+++ scripts/ekf.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python
-from math import nan
-from operator import matmul
+#!/usr/bin/env python3
+from cmath import sqrt
 import rospy
 from rms.msg import *
 from std_msgs.msg import Float64, Int32
@@ -27,10 +26,11 @@ class EKF:
         self.freq = 10.
         T = 1./self.freq
         q = 0.01
-        rx = 10.
-        ry = 10.
-        rz = 30.
-        rh = 0.05
+        rx = 16.
+        ry = 16.
+        rz = 36.
+        rb = 9.
+        rh = 0.001
 
         self.F = np.matrix([[1,  0,  0,  T,  0,  0],
                             [0,  1,  0,  0,  T,  0],
@@ -92,16 +92,41 @@ class EKF:
 
         self.rate = rospy.Rate(self.freq)
         while not rospy.is_shutdown():
+
+            self.R = np.matrix([[rx, 0,  0,  0,  0,  0,  0,  0],
+                                [0,  ry, 0,  0,  0,  0,  0,  0],
+                                [0,  0,  rz, 0,  0,  0,  0,  0],
+                                [0,  0,  0,  rx, 0,  0,  0,  0],
+                                [0,  0,  0,  0,  ry, 0,  0,  0],
+                                [0,  0,  0,  0,  0,  rz, 0,  0],
+                                [0,  0,  0,  0,  0,  0,  rh, 0],
+                                [0,  0,  0,  0,  0,  0,  0,  rh]])
+
+            self.H = np.matrix([[1,  0,  0,  0,  0,  0],
+                                [0,  1,  0,  0,  0,  0],
+                                [0,  0,  1,  0,  0,  0],
+                                [1,  0,  0, -T,  0,  0],
+                                [0,  1,  0,  0, -T,  0],
+                                [0,  0,  1,  0,  0, -T],
+                                [0,  0,  1,  0,  0,  0],
+                                [0,  0,  1,  0,  0, -T]])
             
             # Model selection
-            if (self.state == 0 or self.state == 2): # Static in both cases
+            if (self.state == 0 or self.state == 2): # If landed or idle no movement is predicted and velocity is zero
                 self.F = np.matrix([[1,  0,  0,  0,  0,  0],
                                     [0,  1,  0,  0,  0,  0],
                                     [0,  0,  1,  0,  0,  0],
-                                    [0,  0,  0,  1,  0,  0],
-                                    [0,  0,  0,  0,  1,  0],
+                                    [0,  0,  0,  0,  0,  0],
+                                    [0,  0,  0,  0,  0,  0],
+                                    [0,  0,  0,  0,  0,  0]])
+            elif (self.state == 1 or self.state == 4): # If taking off or landing, only movement in z is predicted with constant vz
+                self.F = np.matrix([[1,  0,  0,  0,  0,  0],
+                                    [0,  1,  0,  0,  0,  0],
+                                    [0,  0,  1,  0,  0,  T],
+                                    [0,  0,  0,  0,  0,  0],
+                                    [0,  0,  0,  0,  0,  0],
                                     [0,  0,  0,  0,  0,  1]])
-            else:
+            else: # If moving, suppose movement in all axes at constant speed
                 self.F = np.matrix([[1,  0,  0,  T,  0,  0],
                                     [0,  1,  0,  0,  T,  0],
                                     [0,  0,  1,  0,  0,  T],
@@ -114,9 +139,6 @@ class EKF:
             P_p = np.matmul(np.matmul(self.F, self.P), np.transpose(self.F)) + self.Q
 
             # UPDATE
-            temp = np.linalg.inv(np.matmul(np.matmul(self.H, P_p), np.transpose(self.H)) + self.R)
-            K = np.matmul(np.matmul(P_p, np.transpose(self.H)), temp)
-
             z = np.matrix([[self.gpsData[0]],
                            [self.gpsData[1]],
                            [self.gpsData[2]],
@@ -124,10 +146,35 @@ class EKF:
                            [self.gpsDataOld[1]],
                            [self.gpsDataOld[2]],
                            [self.heightData],
-                           [self.heightDataOld] ])
+                           [self.heightDataOld]])
 
-            self.X = X_p + np.matmul(K, z - matmul(self.H, X_p))
+            if(self.beaconDists): # If beacon measures
+                for key, value in self.beaconDists.items():
+                    z = np.vstack([z, np.matrix([value])])
+                    
+                    x = float(self.X[0][0])
+                    y = float(self.X[1][0])
+                    z = float(self.X[2][0])
+                    xb = float((self.beaconPoses[key])[0])
+                    yb = float((self.beaconPoses[key])[1])
+                    zb = float((self.beaconPoses[key])[2])
+                    self.H = np.vstack([self.H, np.array([np.real((2*x - 2*xb)/(2*sqrt((x-xb)**2 + (y-yb)**2 + (z-zb)**2))), 
+                                                           np.real((2*y - 2*yb)/(2*sqrt((x-xb)**2 + (y-yb)**2 + (z-zb)**2))),
+                                                           np.real((2*z - 2*zb)/(2*sqrt((x-xb)**2 + (y-yb)**2 + (z-zb)**2))), 0, 0, 0])])
+                    self.R = np.pad(self.R, ((0,1), (0,1)), mode='constant', constant_values=0)
+                    self.R[len(self.R)-1][len(self.R)-1] = rb
+
+            #  [(2*x - 2*xb1)/(2*((x - xb1)^2 + (y - yb1)^2 + (z - zb1)^2)^(1/2)),
+            #   (2*y - 2*yb1)/(2*((x - xb1)^2 + (y - yb1)^2 + (z - zb1)^2)^(1/2)),
+            #   (2*z - 2*zb1)/(2*((x - xb1)^2 + (y - yb1)^2 + (z - zb1)^2)^(1/2)),  0,  0,  0]       
+
+            temp = np.linalg.inv(np.matmul(np.matmul(self.H, P_p), np.transpose(self.H)) + self.R)
+            K = np.matmul(np.matmul(P_p, np.transpose(self.H)), temp)
+
+            self.X = X_p + np.matmul(K, z - np.matmul(self.H, X_p))
             self.P = P_p - np.matmul(np.matmul(K, self.H), P_p)
+
+            #
 
             self.heightDataOld = self.heightData
             self.gpsDataOld = self.gpsData
@@ -141,6 +188,8 @@ class EKF:
             msg.twist.linear.y = self.X[4][0]
             msg.twist.linear.z = self.X[5][0]
             self.pub.publish(msg)
+
+            self.beaconDists.clear()
             
             self.rate.sleep()
 
